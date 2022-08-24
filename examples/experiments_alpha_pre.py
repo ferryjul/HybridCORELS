d7bdb8cad1d924b3b9546202ce10bce81ff2d3a6
julienferry12@gmail.com <julienferry12@gmail.com>
2022-08-24 14:26:45 +0200

    reorganize alpha experiments to train each interpretable part only once

--- examples/experiments_alpha_pre.py
+++ examples/experiments_alpha_pre.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from HybridCORELS import *
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split #KFold
 from local_config import ccanada_expes
 from local_config import ccanada_expes
 
@@ -33,35 +33,48 @@ dataset = args.dataset
 # Prepare parameters (min coverage and alpha value)
 min_coverages = np.concatenate([np.arange(0, 1.0, 0.05), np.arange(0.96, 0.99, 0.01)])
 alpha_values = np.arange(0, 11, 1)
-params_list = [] # 264
+n_seeds = 10
+train_proportion = 0.7
+
+params_list = [] # 240
 for min_coverage in min_coverages:
-    for alpha_value in alpha_values:
-        params_list.append([min_coverage, alpha_value])
-min_coverage, alpha_value = params_list[rank]
+    for seed in range(n_seeds):
+        params_list.append([min_coverage, seed])
+#print("# params = ", len(params_list))
+min_coverage, seed = params_list[rank]
 
 # Set fixed parameters
 random_state_param = 42
 corels_params = {'policy':"lower_bound", 'max_card':1, 'c':0.001, 'n_iter':10**7, 'min_support':0.05, 'verbosity':[]} #"progress"
-n_folds = 5
+#n_folds = 5
 
 # Load and prepare data
 X, y, features, prediction = load_from_csv("data/%s.csv" %dataset) 
-kf = KFold(n_splits=n_folds, shuffle=True, random_state=random_state_param)
+#kf = KFold(n_splits=n_folds, shuffle=True, random_state=random_state_param)
 
 # Generate train and test sets and run the algorithm on each fold
-fold_id = 1
+#fold_id = 1
 res = []
-for train_index, test_index in kf.split(X):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
 
-    bbox = RandomForestClassifier(random_state=42, min_samples_leaf=10, max_depth=10)
-    beta_value = 1 / X_train.shape[0]
+#train_index, test_index in kf.split(X):
+#X_train, X_test = X[train_index], X[test_index]
+#y_train, y_test = y[train_index], y[test_index]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1.0 - train_proportion, 
+                                                shuffle=True, random_state=seed)
 
-    # Create and train the hybrid model
-    hyb_model = HybridCORELSPreClassifier(black_box_classifier=bbox, beta=beta_value, alpha=alpha_value, min_coverage=min_coverage, lb_mode='tight', **corels_params)
-    hyb_model.fit(X_train, y_train, features=features, prediction_name=prediction)
-    
+
+bbox = RandomForestClassifier(random_state=seed, min_samples_leaf=5, max_depth=10)
+beta_value = 1 / X_train.shape[0]
+
+# Create and train the hybrid model
+hyb_model = HybridCORELSPreClassifier(black_box_classifier=bbox, beta=beta_value, alpha=0, min_coverage=min_coverage, lb_mode='tight', **corels_params)
+hyb_model.fit(X_train, y_train, features=features, prediction_name=prediction)
+
+# Iterate over alpha values
+for alpha_value in alpha_values:
+    bbox = RandomForestClassifier(random_state=seed, min_samples_leaf=5, max_depth=10)
+    hyb_model.refit_black_box(X_train, y_train, alpha_value, bbox)
+    #print(alpha_value, hyb_model)
     # Evaluate it
     sparsity = hyb_model.get_sparsity()
     model_str = hyb_model.__str__()
@@ -78,8 +91,10 @@ for train_index, test_index in kf.split(X):
     preds_types_counts_test = np.unique(preds_types_test, return_counts=True)
     index_one_test = np.where(preds_types_counts_test[0] == 1)
     cover_rate_test = preds_types_counts_test[1][index_one_test][0]/np.sum(preds_types_counts_test[1])
-    res.append([fold_id, min_coverage, alpha_value, accuracy_train, cover_rate_train, accuracy_test, cover_rate_test, status, sparsity, model_str])
-    fold_id += 1
+    res.append([seed, min_coverage, alpha_value, accuracy_train, cover_rate_train, accuracy_test, cover_rate_test, status, sparsity, model_str])
+
+
+    #fold_id += 1
 
 # Gather the results for the 5 folds on process 0
 if ccanada_expes:
