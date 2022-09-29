8f53f0ac1a21ceacada51eab6ddb43bfd86f32b5
julienferry12@gmail.com <julienferry12@gmail.com>
2022-09-29 15:57:14 +0200

    adds seed for dataset splitting

--- examples/exp_utils.py
+++ examples/exp_utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from rule_mining import mine_rules_preprocessing
 
-def get_data(dataset, max_card=2, min_support=1, n_rules=300):
+def get_data(dataset, max_card=2, min_support=1, n_rules=300, random_state_param=42):
     df = pd.read_csv(f"data/{dataset}.csv", sep = ',')
     X = df.iloc[:, :-1]
     prediction = df.iloc[:, -1].name # ajout julien
@@ -15,7 +15,6 @@ def get_data(dataset, max_card=2, min_support=1, n_rules=300):
     X = np.array(X) # ajout julien
 
     # Generate train and test sets
-    random_state_param = 42
     train_proportion = 0.8
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1.0 - train_proportion, 
                                                         shuffle=True, random_state=random_state_param+1)
