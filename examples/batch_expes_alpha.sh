258975282b669c326de856a0dd5b73d52e8c2870
julienferry12@gmail.com <julienferry12@gmail.com>
2022-10-07 14:55:05 +0200

    better with btach file

--- examples/batch_expes_alpha.sh
+++ examples/batch_expes_alpha.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -n 180
-#SBATCH --mem-per-cpu=10000
-#SBATCH --time=15:00:00
+#SBATCH -n 450
+#SBATCH --mem-per-cpu=6500
+#SBATCH --time=1:30:00
 #SBATCH --job-name=expes_hycopre
 #SBATCH -o slurm_out/slurmout_%A.out
 #SBATCH -e slurm_out/slurmout_%A.errarray
 #SBATCH --array=0,1
 
-srun -W 54000 -n 180 python3.10 expes_hybridcorelspre.py --dataset=${SLURM_ARRAY_TASK_ID}
+srun -W 4800 -n 450 python3.10 3_1_learn_best_prefixes.py --dataset=${SLURM_ARRAY_TASK_ID}
