e91f9a3c46b512958b6c8f8448b59273a60b6878
galabc <gabriel.laberge@polymtl.ca>
2022-10-19 12:46:36 -0400

    Cache the mined datasets.

--- examples/exp_utils.py
+++ examples/exp_utils.py
@@ -1,20 +1,185 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 from rule_mining import mine_rules_preprocessing
+
+import os 
+import numpy as np 
+
+random_state_value = 42
+test_size_ratio = 0.3
+
+# # Load the new Adult dataset (using the provided folktables module)
+# from folktables import ACSDataSource, ACSEmployment, employment_filter
+
+
+# categories = {
+#     "AGEP" : {
+#         1.0 : "age_low",
+#         2.0 : "age_medium",
+#         3.0 : "age_high"
+#     },
+#     "SCHL" : {
+#         1.0: "No schooling completed",
+#         2.0: "Nursery school, preschool",
+#         3.0: "Kindergarten",
+#         4.0: "Grade 1",
+#         5.0: "Grade 2",
+#         6.0: "Grade 3",
+#         7.0: "Grade 4",
+#         8.0: "Grade 5",
+#         9.0: "Grade 6",
+#         10.0: "Grade 7",
+#         11.0: "Grade 8",
+#         12.0: "Grade 9",
+#         13.0: "Grade 10",
+#         14.0: "Grade 11",
+#         15.0: "12th grade - no diploma",
+#         16.0: "Regular high school diploma",
+#         17.0: "GED or alternative credential",
+#         18.0: "Some college, but less than 1 year",
+#         19.0: "1 or more years of college credit, no degree",
+#         20.0: "Associate's degree",
+#         21.0: "Bachelor's degree",
+#         22.0: "Master's degree",
+#         23.0: "Professional degree beyond a bachelor's degree",
+#         24.0: "Doctorate degree",
+#     },
+#     "MAR": {
+#         1.0: "Married",
+#         2.0: "Widowed",
+#         3.0: "Divorced",
+#         4.0: "Separated",
+#         5.0: "Never married or under 15 years old",
+#     },
+#     "SEX": {1.0: "Male", 2.0: "Female"},
+#     "RAC1P": {
+#         1.0: "White alone",
+#         2.0: "Black or African American alone",
+#         3.0: "American Indian alone",
+#         4.0: "Alaska Native alone",
+#         5.0: (
+#             "American Indian and Alaska Native tribes specified;"
+#             "or American Indian or Alaska Native,"
+#             "not specified and no other"
+#         ),
+#         6.0: "Asian alone",
+#         7.0: "Native Hawaiian and Other Pacific Islander alone",
+#         8.0: "Some Other Race alone",
+#         9.0: "Two or More Races",
+#     },
+#     "ESP" : {
+#         0.0 : "N/A (not own child of householder, and not child in subfamily)",
+#         1.0 : "Living with two parent : both employed",
+#         2.0 : "Living with two parent : Father employed",
+#         3.0 : "Living with two parent : Mother employed",
+#         4.0 : "Living with two parent : None employed",
+#         5.0 : "Living with Father : Employed",
+#         6.0 : "Living with Father : Not employed",
+#         7.0 : "Living with Mother : Employed",
+#         8.0 : "Living with Mother : Not employed",
+#     },
+#     "DIS" : {
+#         1.0 : "Disability",
+#         2.0 : "No disability"
+#     },
+#     "NATIVITY" : {
+#         1.0 : "Native",
+#         2.0 : "Foreign born"
+#     },
+#     "DREM" : {
+#         1.0 : "Cognitive difficulty",
+#         2.0 : "No Cognitive difficulty"
+#     },
+#     "RELP" : {
+#         0.0 : "Reference person",
+#         1.0 : "Husband/wife",
+#         2.0 : "Biological son or daughter",
+#         3.0 : "Adopted son or daughter",
+#         4.0 : "Stepson or stepdaughter",
+#         5.0 : "Brother or sister",
+#         6.0 : "Father or mother",
+#         7.0 : "Grandchild",
+#         8.0 : "Parent-in-law",
+#         9.0 : "Son-in-law or daughter-in-law",
+#         10.0 : "Other relative",
+#         11.0 : "Roomer or boarder",
+#         12.0 : "Housemate or roommate",
+#         13.0 : "Unmarried partner",
+#         14.0 : "Foster child",
+#         15.0 : "Other nonrelative",
+#         16.0 : "Institutionalized group quarters population",
+#         17.0 : "Noninstitutionalized group quarters population"
+#     }
+# }
+
+
+# def binarize_age(X):
+#     quantiles = np.quantile(X[:, 0], [0, 0.33, 0.66, 1])
+#     for q in range(3):
+#         index = np.where((quantiles[q] <= X[:, 0]) & (X[:, 0] <= quantiles[q+1]))[0]
+#         X[index, 0] = q+1
+
+
+# def generate_acs_data():
+#     data_source = ACSDataSource(survey_year='2018', horizon='1-Year', survey='person')
+#     acs_data = data_source.get_data(states=["TX"], download=True)
+#     acs_data = employment_filter(acs_data)
+#     features = ACSEmployment.features
+#     X, y, _ = ACSEmployment.df_to_numpy(acs_data)
+#     binarize_age(X)
+
+#     # Reorder features according to categories dict
+#     keep_features = list(categories.keys())
+#     keep_features_idx = [ features.index(f) for f in keep_features]
+#     X = X[:, keep_features_idx]
+
+#     # OHE the categorical features
+#     ohe = OneHotEncoder(sparse=False).fit_transform(X)
+#     ohe_features = []
+#     for feature_cat in categories.values():
+#         ohe_features += list(feature_cat.values())
+
+#     # Save the dataset
+#     ohe_df = pd.DataFrame(np.column_stack((ohe, y)).astype(int), 
+#                          columns=ohe_features+["Employed"])
+#     filename = os.path.join("data", "acs_employ.csv")
+#     ohe_df.to_csv(filename, encoding='utf-8', index=False)
 
 
 
 def get_data(dataset, splits, max_card=2, min_support=1, n_rules=300, random_state_param=42):
-    df = pd.read_csv(f"data/{dataset}.csv", sep = ',')
-    X = df.iloc[:, :-1]
-    prediction = df.iloc[:, -1].name
+    # # Generate the acs_data if it is not already there
+    # if dataset == "acs_employ":# and not os.path.exists(f"data/{dataset}.csv"):
+    #     generate_acs_data()
 
-    y = np.array(df.iloc[:, -1])
-    X = mine_rules_preprocessing(X, y, max_card, min_support, n_rules)
-    features = list(X.columns)
+    # Mine the dataset set if it has not already been done
+    if not os.path.exists(f"data/{dataset}_mined.csv"):
 
-    X = np.array(X)
+        df = pd.read_csv(f"data/{dataset}.csv", sep = ',')
+        X = df.iloc[:, :-1]
+        prediction = df.iloc[:, -1].name
+        y = np.array(df.iloc[:, -1])
+
+        # Mine the rules
+        X = mine_rules_preprocessing(X, y, max_card, min_support, n_rules)
+        features = list(X.columns)
+        X = np.array(X)
+
+        # Save the dataset
+        df = pd.DataFrame(np.column_stack((X, y)), columns=features+[prediction])
+        df.to_csv(f"data/{dataset}_mined.csv", encoding='utf-8', index=False)
+
+    # Rules have already been mined
+    else:
+        df = pd.read_csv(f"data/{dataset}_mined.csv", sep = ',')
+        X = df.iloc[:, :-1]
+        features = list(X.columns)
+        X = np.array(X)
+        prediction = df.iloc[:, -1].name
+        y = np.array(df.iloc[:, -1])
+
 
     # Generate splits
     assert len(splits) <= 3, "We only support splitting the data to up to 3 folds"
