0307ebcb68850ab32d6b99db338952aaf2510c29
julienferry12@gmail.com <julienferry12@gmail.com>
2022-10-20 20:51:30 +0200

    want to add former value of lambda, running partially experiments

--- examples/3_1_learn_prefixes_batch.sh
+++ examples/3_1_learn_prefixes_batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -n 450
+#SBATCH -n 225
 #SBATCH --mem-per-cpu=9000
 #SBATCH --time=02:00:00
 #SBATCH --job-name=expes_hycopre
@@ -8,4 +8,4 @@
 #SBATCH -e slurm_out/slurmout_%A.errarray
 #SBATCH --array=0,1,2
 
-srun -W 7200 -n 450 python3.10 3_1_learn_best_prefixes.py --dataset=${SLURM_ARRAY_TASK_ID}
+srun -W 7200 -n 225 python3.10 3_1_learn_best_prefixes.py --dataset=${SLURM_ARRAY_TASK_ID}
