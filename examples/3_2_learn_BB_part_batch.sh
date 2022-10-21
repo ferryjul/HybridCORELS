57a3792e195d7b99f5ec9ad19420f1a74d9eac45
julienferry12@gmail.com <julienferry12@gmail.com>
2022-10-21 14:29:28 +0200

    phase 2 ready

--- examples/3_2_learn_BB_part_batch.sh
+++ examples/3_2_learn_BB_part_batch.sh
@@ -6,6 +6,6 @@
 #SBATCH --job-name=expes_hycopre
 #SBATCH -o slurm_out/slurmout_%A.out
 #SBATCH -e slurm_out/slurmout_%A.errarray
-#SBATCH --array=0,1
+#SBATCH --array=0,1,2
 
 srun -W 3600 -n 275 python3.10 3_2_learn_BB_part.py --dataset=${SLURM_ARRAY_TASK_ID}
