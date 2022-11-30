396016e2b5beb866262329f60a11df4166aa1ea7
julienferry12@gmail.com <julienferry12@gmail.com>
2022-11-30 19:24:39 +0100

    Prepares scripts for phase 2 of expes

---
+++ examples/4_1_learn_post_black_boxes_batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -n 15
+#SBATCH --mem-per-cpu=9000
+#SBATCH --time=02:00:00
+#SBATCH --job-name=expes_post
+#SBATCH -o slurm_out/slurmout_%A.out
+#SBATCH -e slurm_out/slurmout_%A.errarray
+#SBATCH --array=0,1,2
+
+srun -W 7200 -n 15 python3.10 3_1_learn_best_prefixes.py --dataset=${SLURM_ARRAY_TASK_ID}
