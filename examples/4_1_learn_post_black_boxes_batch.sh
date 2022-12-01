9e14fa111063d5f424cb95a85b025f64ffc73390
julienferry12@gmail.com <julienferry12@gmail.com>
2022-12-01 19:01:16 +0100

    phase 4_1_ test ready

--- examples/4_1_learn_post_black_boxes_batch.sh
+++ examples/4_1_learn_post_black_boxes_batch.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -n 15
 #SBATCH --mem-per-cpu=9000
-#SBATCH --time=02:00:00
+#SBATCH --time=03:00:00
 #SBATCH --job-name=expes_post
 #SBATCH -o slurm_out/slurmout_%A.out
 #SBATCH -e slurm_out/slurmout_%A.errarray
