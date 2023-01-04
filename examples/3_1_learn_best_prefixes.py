1068cfd014c96e44f1a2a2659dde33d0280182ea
julienferry12@gmail.com <julienferry12@gmail.com>
2023-01-04 12:16:07 +0100

    prepare to relaunch experiments phase 3 in collab mode

--- examples/3_1_learn_best_prefixes.py
+++ examples/3_1_learn_best_prefixes.py
@@ -81,13 +81,13 @@ black_box="fixed_rf"
 
 # Create the hybrid model
 bbox = RandomForestClassifier(random_state=42, min_samples_split=10, max_depth=10) #useless
-hyb_model = HybridCORELSPreClassifier(black_box_classifier=bbox, beta=beta_value, alpha=alpha_value, min_coverage=min_coverage, **corels_params)
+hyb_model = HybridCORELSPreClassifier(black_box_classifier=bbox, beta=beta_value, alpha=alpha_value, min_coverage=min_coverage, obj_mode='collab', **corels_params)
 
 # Train the hybrid model
 hyb_model.fit(X_train, y_train, features=features, prediction_name=prediction, time_limit=interpr_tout, memory_limit=interpr_mem)
 
 # Compute and save all metrics
-hyb_model.save("./models_save/prefix_%s_%d_%.3f_%.5f_%d_%.2f_%s" %(dataset_name, random_state_value, min_coverage, cValue, n_iter_param, min_support_param, policy))
+hyb_model.save("./models_save/prefix_%s_%d_%.3f_%.5f_%d_%.2f_%s_collab" %(dataset_name, random_state_value, min_coverage, cValue, n_iter_param, min_support_param, policy))
 
 # Train set
 preds_train, preds_types_train = hyb_model.predict_with_type(X_train)
@@ -105,6 +105,8 @@ black_box_acc_upper_bound = computeAccuracyUpperBound(X_train[bb_indices_train],
 if not ccanada_expes:
     print("Accuracy upper bound = ", black_box_acc_upper_bound)
 
+train_acc_ub = (interpr_accuracy_train * transparency_train) + (black_box_acc_upper_bound * (1 - transparency_train))
+
 # Test set
 preds_test, preds_types_test = hyb_model.predict_with_type(X_test)
 test_acc = np.mean(preds_test == y_test)
@@ -119,7 +121,7 @@ interpr_accuracy_test = np.mean(preds_test[interpr_indices_test] == y_test[inter
 status = hyb_model.get_status()
 sparsity = hyb_model.get_sparsity()
 print("Expe: dataset = %s, " %dataset_name, "min_cov=%s" %min_coverage, "policy=%s" %policy, "c=%.3f" %cValue, "bb=%s"%black_box, "alpha=%.3f" %alpha_value, " done.")
-res.append([random_state_value, black_box, min_coverage, beta_value, alpha_value, policy, min_support_param, cValue, status, train_acc, test_acc, interpr_accuracy_train, interpr_accuracy_test, black_box_accuracy_train, black_box_acc_upper_bound, black_box_accuracy_test, transparency_train, transparency_test, str(hyb_model), sparsity])
+res.append([random_state_value, black_box, min_coverage, beta_value, alpha_value, policy, min_support_param, cValue, status, train_acc, test_acc, interpr_accuracy_train, interpr_accuracy_test, black_box_accuracy_train, black_box_acc_upper_bound, black_box_accuracy_test, transparency_train, transparency_test, str(hyb_model), sparsity, train_acc_ub])
 
 # Gather the results for the 5 folds on process 0
 if ccanada_expes:
@@ -127,11 +129,11 @@ if ccanada_expes:
 
 if rank == 0 or not ccanada_expes:
     # save results
-    fileName = './results/results_HybridCORELSPre_%s.csv' %(dataset_name) #_proportions
+    fileName = './results/results_HybridCORELSPreCollab_%s.csv' %(dataset_name) #_proportions
     import csv
     with open(fileName, mode='w') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(['Seed', 'Black Box', 'Min coverage', 'Beta', 'Alpha', 'Policy', 'Min support', 'Lambda', 'Search status', 'Training accuracy', 'Test accuracy', 'Training accuracy (prefix)', 'Test accuracy (prefix)', 'Training accuracy (BB)', 'Training accuracy (BB) UB', 'Test accuracy (BB)', 'Training transparency', 'Test transparency', 'Model', 'Prefix length'])
+        csv_writer.writerow(['Seed', 'Black Box', 'Min coverage', 'Beta', 'Alpha', 'Policy', 'Min support', 'Lambda', 'Search status', 'Training accuracy', 'Test accuracy', 'Training accuracy (prefix)', 'Test accuracy (prefix)', 'Training accuracy (BB)', 'Training accuracy (BB) UB', 'Test accuracy (BB)', 'Training transparency', 'Test transparency', 'Model', 'Prefix length', 'Train acc UB'])
         for i in range(len(res)):
             if ccanada_expes:
                 for j in range(len(res[i])):
